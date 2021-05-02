033bb796bd33ec7ad135d516d4badfaccd8b641d
Andrey Kudryavtsev <lomastering@gmail.com>
2021-05-02 14:48:07 +0300

    feat: nav

--- src/components/StepsNav/StepsNav.tsx
+++ src/components/StepsNav/StepsNav.tsx
@@ -1,5 +1,38 @@
 import { FC } from 'react';
 
-const StepsNav: FC = () => <></>;
+import styles from './StepsNav.module.scss';
+
+const StepsNav: FC = () => (
+  <nav className={styles['steps-nav']}>
+    <ul className={styles['steps-nav__list']}>
+      <li>
+        <button
+          className={`${styles['steps-nav__link']} ${styles['steps-nav__link_active']} ${styles['steps-nav__link_normal']}`}
+          type="button"
+        >
+          Shipping
+        </button>
+      </li>
+      <li className={styles['steps-nav__divider']} />
+      <li>
+        <button
+          type="button"
+          className={`${styles['steps-nav__link']} ${styles['steps-nav__link_disabled']}`}
+        >
+          Billing
+        </button>
+      </li>
+      <li className={styles['steps-nav__divider']} />
+      <li>
+        <button
+          type="button"
+          className={`${styles['steps-nav__link']} ${styles['steps-nav__link_disabled']}`}
+        >
+          Payment
+        </button>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default StepsNav;
--- src/containers/OrderFormContainer/OrderFormContainer.tsx
+++ src/containers/OrderFormContainer/OrderFormContainer.tsx
@@ -1,9 +1,12 @@
 import { FC } from 'react';
 
+import StepsNav from '../../components/StepsNav/StepsNav';
+
 import styles from './OrderFormContainer.module.scss';
 
 const OrderFormContainer: FC = () => (
   <div className={styles.form}>
+    <StepsNav />
     <h2 className={styles.form__title}>Shipping Info</h2>
   </div>
 );
