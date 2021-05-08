e451ace02098aaa0d96ca6ce3b58feee4577932a
Andrey Kudryavtsev <lomastering@gmail.com>
2021-05-08 18:40:38 +0300

    feat: visa detect

--- src/components/FormInput/FormInputCard.tsx
+++ src/components/FormInput/FormInputCard.tsx
@@ -1,5 +1,5 @@
 import { FieldHookConfig, useField } from 'formik';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useRef } from 'react';
 
 import styles from './FormInput.module.scss';
 
@@ -7,16 +7,22 @@ const FormInputCard = (props: FieldHookConfig<string>) => {
   const [field, meta, helpers] = useField(props);
 
   const { type, placeholder } = props;
+  const { value } = meta;
   const { onChange, ...fieldProps } = field;
   const { setValue } = helpers;
 
+  const input = useRef<HTMLInputElement>(null);
+
   const formatCard = (evt: ChangeEvent<HTMLInputElement>) => {
     onChange(evt);
-    let { value } = evt.target;
-    if (value.length > 4) {
-      value = value.replace(/\D/g, '').match(/.{1,4}/g)!.join(' ');
+    let currentValue = evt.target.value;
+    if (currentValue.length > 4) {
+      currentValue = currentValue
+        .replace(/\D/g, '')
+        .match(/.{1,4}/g)!
+        .join(' ');
     }
-    setValue(value);
+    setValue(currentValue);
   };
 
   return (
@@ -27,11 +33,13 @@ const FormInputCard = (props: FieldHookConfig<string>) => {
 
       <input
         className={`${styles.input__field}
+          ${value[0] === '4' ? styles.input__field_visa : ''}
           ${meta.touched && meta.error ? styles.input__field_error : ''}`}
         type={type}
         placeholder={placeholder}
         onChange={formatCard}
         maxLength={19}
+        ref={input}
         {...fieldProps}
       />
     </div>
